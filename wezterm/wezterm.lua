e856e86f2bbdbe59f1a5896d6c3c22f95e5176d9
Thorsten Hans <thorsten.hans@fermyon.com>
2025-08-16 13:20:23 +0200

    chore: Update WezTerm config
    Do not activate full-screen mode on startup as this does not work well
    with tiling window manager
    
    Signed-off-by: Thorsten Hans <thorsten.hans@fermyon.com>

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,17 +6,20 @@ local action = wezterm.action
 local config = {}
 
 -- Fonts
-config.font = wezterm.font("JetBrains Mono ExtraLight")
-config.font_size = 15
+config.font = wezterm.font({
+	family = "DankMono Nerd Font",
+	weight = "Regular",
+})
+config.font_size = 22
 
 -- Command Palette
 config.command_palette_bg_color = "#232634"
 config.command_palette_fg_color = "#babbf1"
-config.command_palette_font_size = 16
-config.command_palette_rows = 16
+config.command_palette_font_size = 20
+config.command_palette_rows = 20
 
 -- Cursor
-config.cursor_blink_rate = 350
+config.cursor_blink_rate = 250
 config.cursor_blink_ease_in = "Linear"
 config.default_cursor_style = "BlinkingBar"
 
@@ -31,7 +34,7 @@ config.inactive_pane_hsb = {
 	brightness = 0.6,
 }
 config.window_frame = {
-	font = wezterm.font({ family = "JetBrains Mono", weight = "Medium" }),
+	font = wezterm.font({ family = "DankMono NerdFont", weight = "Regular" }),
 	font_size = 14,
 	active_titlebar_bg = "#303446",
 	inactive_titlebar_bg = "#303446",
@@ -62,23 +65,23 @@ config.colors = {
 	},
 }
 config.window_padding = {
-	left = 9,
-	right = 9,
-	top = 9,
-	bottom = 9,
+	left = 50,
+	right = 50,
+	top = 25,
+	bottom = 25,
 }
 config.window_decorations = "RESIZE"
 
 -- Default Dimensions
 config.initial_rows = 40
 config.initial_cols = 130
-config.window_background_opacity = 0.99
+config.window_background_opacity = 0.93
 
 -- wezterm events
-wezterm.on("gui-startup", function(cmd)
-	local _, _, window = mux.spawn_window(cmd or {})
-	window:gui_window():toggle_fullscreen()
-end)
+-- wezterm.on("gui-startup", function(cmd)
+--	local _, _, window = mux.spawn_window(cmd or {})
+--	window:gui_window():toggle_fullscreen()
+-- end)
 
 -- Misc
 config.window_close_confirmation = "NeverPrompt"
@@ -95,6 +98,11 @@ config.keys = {
 		key = "DownArrow",
 		mods = "CMD|SHIFT",
 		action = action.ToggleFullScreen,
+	},
+	{
+		key = "=",
+		mods = "CMD|SHIFT",
+		action = action.ResetFontSize,
 	},
 }
 return config
