407bbf33d224737aa8fcca94d2b62a77bf9162cc
Thorsten Hans <thans@akamai.com>
2026-06-11 10:45:27 +0200

    chore: Synthwave Everything!
    Signed-off-by: Thorsten Hans <thans@akamai.com>

--- nvim/lua/thns/plugins/theme.lua
+++ nvim/lua/thns/plugins/theme.lua
@@ -1,18 +1,7 @@
 return {
   {
-    "folke/tokyonight.nvim",
+    "samharju/synthweave.nvim",
     priority = 1000,
-    opts = {
-      style = "storm",
-      on_highlights = function(hl, c)
-        -- bg_dark is already the storm default for floats, so go darker to stand out
-        local float_bg = "#13141d"
-        hl.NormalFloat      = { bg = float_bg, fg = c.fg }
-        hl.FloatBorder      = { bg = float_bg, fg = c.blue }
-        -- Noice renders hover via its own groups; set them explicitly
-        hl.NoicePopup       = { bg = float_bg, fg = c.fg }
-        hl.NoicePopupBorder = { bg = float_bg, fg = c.blue }
-      end,
-    },
+    opts = {},
   },
 }
