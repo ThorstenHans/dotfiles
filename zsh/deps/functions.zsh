562ea0bbe6be3fa7b7f3af09c260b3422ce113e7
Thorsten Hans <thans@akamai.com>
2026-06-11 09:51:47 +0200

    chore: Add function for loading kubeconfig from lke by cluster name
    Signed-off-by: Thorsten Hans <thans@akamai.com>

--- zsh/deps/functions.zsh
+++ zsh/deps/functions.zsh
@@ -11,6 +11,43 @@ function guid()
 	echo $id
 }
 
+function get_lke_kubeconfig() {
+  local cluster_name="${1:?Error: Please provide a cluster name}"
+
+  print -P "\n%F{cyan}[~] Looking for LKE cluster '%B${cluster_name}%b'...%f"
+
+  local cluster_ids=("${(@f)$(linode lke clusters-list --label "$cluster_name" --json | jq -r '.[].id')}")
+
+  if [[ -z "${cluster_ids[1]}" ]]; then
+    print -P "%F{red}[✘] No cluster found with label '${cluster_name}'.%f\n"
+    return 1
+  fi
+
+  if [[ ${#cluster_ids[@]} -gt 1 ]]; then
+    print -P "%F{yellow}[!] Multiple clusters found matching '${cluster_name}':%f"
+    local cluster_names=("${(@f)$(linode lke clusters-list --label "$cluster_name" --json | jq -r '.[].label')}")
+    for name in $cluster_names; do
+      print -P "  %F{yellow}• ${name}%f"
+    done
+    print -P "%F{yellow}Please use a more specific name.%f\n"
+    return 1
+  fi
+
+  local cluster_id="${cluster_ids[1]}"
+  print -P "%F{green}[✔] Cluster found (ID: ${cluster_id}).%f"
+
+  print -P "%F{cyan}[~] Appending kubeconfig...%f"
+
+  if linode lke kubeconfig-view "$cluster_id" --json | jq -r '.[].kubeconfig | @base64d' >> ~/.kube/config; then
+    print -P "%F{green}[✔] Kubeconfig updated successfully.%f\n"
+  else
+    print -P "%F{red}[✘] Failed to retrieve or append kubeconfig.%f\n"
+    return 1
+  fi
+
+  kubectl config get-contexts
+}
+
 function find_app_by_id() {
   local target_id="${1:?Error: Please provide a Target App ID (GUID)}"
   
